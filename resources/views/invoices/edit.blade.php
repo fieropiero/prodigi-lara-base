563243d6b68ffaad64a0236695f20f9093f44bf2
fieropiero <giacomorelli16@gmail.com>
2023-03-13 17:13:15 +0100

    create invoices views

--- resources/views/invoices/edit.blade.php
+++ resources/views/invoices/edit.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
-@include('partials.form-errors')
+
 
 @section('content')
-<div class='container'>
-    <form method="post" action="{{route('invoices.store')}}">
+@include('partials.form-errors')
+<form method="post" action="{{route('invoices.update', $invoice)}}">
     @csrf
+    @method('put')
     <div class="mb-3">
         <label for="code" class="form-label">Code</label>
         <input type="text" class="form-control" id="code" name="code" placeholder="Type code" value="{{old('code')}}">
@@ -16,13 +17,13 @@
     </div>
     <div class="mb-3">
         <label for="payments" class="form-label">Payments</label>
-        <select class="form-select" aria-label="Default select example"  id="payments" name="payments" >
+        <select class="form-select" aria-label="Default select example"  id="payments" name="payments" value="{{old('payments')}}">
             <option selected>Open this select menu</option>
             <option value="Transfer">Transfer</option>
             <option value="Card">Card</option>
         </select>
     </div>
-        <button class="btn btn-primary">Confirm</button>
-    </form>
-</div>
+    <button class="btn btn-primary">Confirm</button>
+</form>
+
 @endsection
--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,11 +1,18 @@
-@extends('layouts.index')
+@extends('layouts.app')
 
 @section('content')
     <a href="{{route('invoices.create')}}" class="btn btn-primary">Add Invoice</a>
+        
+    @if(session('message'))
+    <div class="alert alert-success mt-3" role="alert">
+        {{session('message')}}
+    </div>
+    @endif
+
 @endsection
 
 @section('table_head')
-    <th>Code</th><th>Total</th><th>Payments</th>
+    <th>Code</th><th>Total</th><th>Payments</th><th></th><th></th>
 @endsection
 
 @section('table_body')
@@ -14,6 +21,14 @@
             <td>{{$invoice->code}}</td>
             <td>{{$invoice->total}}</td>
             <td>{{$invoice->payments}}</td>
+            <td><a href="{{route('invoices.edit', $invoice)}}" class="btn btn-success">Modify</a></td>
+            <td>
+                <form method="post" action="{{route('invoices.destroy', $invoice)}}">
+                @method('delete')
+                @csrf    
+                <button class="btn btn-danger confirm_delete">Cancella</button>
+                </form>
+            </td>
         </tr>
     @endforeach
 @endsection
