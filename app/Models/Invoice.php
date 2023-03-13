b49ec121401710d38956f8e120a011230e4199fd
fieropiero <giacomorelli16@gmail.com>
2023-03-13 17:13:33 +0100

    modified models

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'total',
+        'payments'
+    ];
+
 }
